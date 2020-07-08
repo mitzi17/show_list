4c6c8f57faa4cea6c64cce816aad96c9ae3c180a
Mitzi17 <m_estrada17@live.com>
2020-07-08 00:38:18 -0700

    add class method all

--- lib/show_list/show.rb
+++ lib/show_list/show.rb
@@ -1,6 +1,22 @@
 
 module ShowList
     class Show
+      attr_accessor :name, :description
 
+      
+
+      def initialize(name, description)
+        @name = name
+        @description = description
+      end
+
+      @@all = [
+        Show.new("Friends", "an awesome show about friends in NYC")
+      ]
+
+      def self.all
+        @@all
+      end
     end
-  end
+end
+
